896bce04ca7fac23266ed2ec5ec9294cc662cdbf
ale190288 <ale190288@gmail.com>
2024-11-18 22:24:23 -0300

    Aula 17 Javascript

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function mostraAlerta(){
+    alert("Não é que isso funciona mesmo!!!");
+}
+function mostraAlerta1(msg){
+    alert("Boa noite, "+ msg);
+}
+function mensagem_com_parametro_return(msg1)
+{
+    return("Voce digitou:" + msg1);
+}
+
+function resultadoDaSoma(num1,num2)
+{
+   var num1, num2, soma;
+
+   soma= parseFloat (num1) + parseFloat (num2);
+
+   return(soma);
+}
